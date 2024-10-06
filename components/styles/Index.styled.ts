bd1854f103064d63c4ea6f8327f49ca153f0e2ec
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-10-06 19:35:24 +0200

    add text

---
+++ components/styles/Index.styled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+// import { View } from 'react-native';
+
+export const ViewContainer = styled.View`
+display: flex;   
+ align-content: center;
+ justify-content: center;
+margin: 10%;
+
+`;
+
+export const TextHero = styled.Text`
+font-size: 6rem;
+`;
+
+
+
